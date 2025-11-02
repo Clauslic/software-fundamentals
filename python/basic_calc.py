6dce0de5a6d7544c64227f5f05ae0b9ea5e3a5f6
Clauslic <claudia.cifuentes@correo.tdea.edu.co>
2025-11-02 12:48:15 -0500

    Python script for a basic calculator

---
+++ python/basic_calc.py
@@ -0,0 +1,31 @@
+## Algoritm description
+'''//Basic calc v1
+//Get two numbers
+//Add, subs, mult, div
+//Print results
+'''
+#1.Inicializar vars and/or constants
+num1 = 10
+num2 = 3
+
+#2. Processes
+add = num1 + num2
+subs = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+#3.Outputs
+print("Addition: ",add, type(add)) 
+print("Subtraction: ", subs, type(subs)) 
+print ("Multiplication: ", mult, type(mult))
+print ("Division: ", div, type(div))
+#3.Outputs
+print("/n")
+print(f"Addition: = {add}") 
+print(f"Subtraction: = {subs}") 
+print (f"Multiplication: = {mult} ")
+print (f"Division: = {div}")
+	
+	
+	
+#FinAlgoritmo
